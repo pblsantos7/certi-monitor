bb0dca41db3137a57f979e4640efe8911148c4a0
Pablo <pablorsantosm@gmaiil.com>
2026-09-15 19:50:35 -0300

    feat: cria validação para data de emissão do certificado

--- src/validators/certificate.validator.ts
+++ src/validators/certificate.validator.ts
@@ -1,5 +1,21 @@
 export function validateName(name: string): void{
     if(name.trim().length === 0){
-        throw new Error("Nome está vazio")
+        throw new Error("Nome está vazio!")
+    }
+}
+
+export function validateDataEmission(date: Date): void{
+    if(isNaN(date.getTime())){
+        throw new Error("Data inválida")
+    }
+
+    const dateCopy = new Date(date.getTime())
+    dateCopy.setHours(0, 0, 0, 0)
+
+    const today = new Date()
+    today.setHours(0, 0, 0, 0)
+
+    if(dateCopy> today){
+        throw new Error("Data não pode ser maior do que hoje!")
     }
 }
