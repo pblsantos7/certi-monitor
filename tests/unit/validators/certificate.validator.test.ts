1b7455657fabdd5d6dc56fe641d5d6431504af35
Pablo <pablorsantosm@gmaiil.com>
2026-09-22 17:50:41 -0300

    test: corrige testes da função de validateDateExpiration

--- tests/unit/validators/certificate.validator.test.ts
+++ tests/unit/validators/certificate.validator.test.ts
@@ -58,15 +58,21 @@ describe('validateDateExpiration', () => {
     expect(() => validateDateExpiration(new Date('data-invalida'), new Date('2026-10-08'))).toThrow('Data inválida')
   });
 
+  it('deve lançar erro se a data de expiração for anterior a hoje', () => {
+  const dateExpiration = new Date('2026-07-08')
+  const dateEmission = new Date('2026-07-07')
+  expect(() => validateDateExpiration(dateExpiration, dateEmission)).toThrow('Data de expiração não pode ser anterior a hoje!')
+})
+
   it('deve lançar erro quando data de emissão for maior que a data de expiração', () => {
-    const dateEmission = new Date('2026-10-08')
-    const dateExpiration = new Date('2026-10-07')
+    const dateEmission = new Date('2026-09-26')
+    const dateExpiration = new Date('2026-09-25')
     expect(() => validateDateExpiration(dateExpiration, dateEmission)).toThrow('Data de expiração não pode ser igual ou menor que data de emissão!')
   })
 
   it('deve lançar erro quando a data de emissão for igual a data de expiração', () => {
-    const dateEmission = new Date('2026-10-08')
-    const dateExpiration = new Date('2026-10-08')
+    const dateEmission = new Date('2026-09-25')
+    const dateExpiration = new Date('2026-09-25')
     expect(() => validateDateExpiration(dateExpiration, dateEmission)).toThrow('Data de expiração não pode ser igual ou menor que data de emissão!')
   })
 
